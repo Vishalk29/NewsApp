6089cd2331251d28cba337bde65bd28366d47fd7
Vishal k <vkulkarni149@gmail.com>
2022-01-10 15:38:55 +0530

    creating the constuctor and adding the super inside the constuctor and passing the article variable inside this dot state and adding the newsUrl for detail description of url and

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import News from './Component/News';
 
 export default class App extends Component {
   render() {
+    // document.body.style.background = "gray";
     return (
       <div>
         <NavBar />
